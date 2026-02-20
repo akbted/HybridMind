84d57924c711c01e581a7f89b2fcc0040a6af022
akbted <anantha.b@btech.christuniversity.in>
2026-02-21 00:54:03 +0530

    fix: correct constructor method name in DBClient class

--- backend/config/clients.py
+++ backend/config/clients.py
@@ -1,10 +1,11 @@
 from backend.config.setting import proj_settings, db_settings
 from psycopg2 import pool
+import threading
 
 # DB_CLIENT
 class DBClient:
-    def __int__(self, db_settings):
-        self.connection_pool = db_settings
+    def __init__(self, db_settings):
+        self.connection_pool = db_settings.connection_pool
     
     def get_connection(self):
         return self.connection_pool.getconn()
